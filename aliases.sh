55aa049db1b922353872c13d7c5823acb88808a3
alexeypolo <alexeypolo@gmail.com>
2016-03-10 10:38:45 +0200

    Update aliases.sh

--- aliases.sh
+++ aliases.sh
@@ -1,3 +1,10 @@
 # source me
 
+# launch GDB and automatically run the given command with args
 alias gdbrun="gdb -ex=r --args"
+
+# Git diff with a graphical tool of your choice. Requires to add these two lines in ~/.gitconfig :
+# [diff]
+#        tool = gvimdiff
+alias gdiff='git difftool --no-prompt'
+
